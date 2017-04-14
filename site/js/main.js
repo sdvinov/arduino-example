dfec5202c1139beccd2ffc2fb8959c6584c00c03
sdvinov <odvakr.wikia@gmail.com>
2017-04-14 12:31:11 -0400

    Restyling

--- site/js/main.js
+++ site/js/main.js
@@ -11,26 +11,26 @@ let hours = 0;
 let minutes = 0;
 let seconds = 0;
 
-call.onreadystatechange = function() 
+call.onreadystatechange = function()
 {
 	if (this.readyState != 4 || this.status != 200) {
 		return;
 	}
 	const data = JSON.parse(this.responseText);
-	for(let i = 0; i < data.length; i++) 
+	for(let i = 0; i < data.length; i++)
 	{
 		date = new Date(data[i].timestamp);
 		hours = ("0" + date.getHours()).slice(-2);
 		minutes = ("0" + date.getMinutes()).slice(-2);
 		seconds = ("0" + date.getSeconds()).slice(-2);
-		t_cont += '<tr><td id="c">' + data[i].temp_c + 'C</td><td id="f">' + data[i].temp_f + 'F</td><td id="d">' + hours + ":" + minutes + ":" + seconds + '</td></tr>';
+		t_cont += '<div class="line"><div class="t"><p><img src="img/time.png" />' + hours + ":" + minutes + ":" + seconds + '</p></div><div class="temp"><span class="c">' + data[i].temp_c + ' °C</span><span class="f">' + data[i].temp_f + ' °F</span></div></div>';
 		sum_c += Number(data[i].temp_c);
 		sum_f += Number(data[i].temp_f);
 	}
 	let avg_c = Math.round(sum_c / (data.length));
 	let avg_f = Math.round(sum_f / (data.length));
-	let t_avg = '<tr id="a"><td>Average ' + avg_c.toFixed(2) + 'C</td><td>Average ' + avg_f.toFixed(2) + 'F</td><td></td></tr>';
-	output = "<table>" + t_avg + t_cont + "</table>";
+	let t_avg = '<div id="a"><p id="c">Average ' + avg_c.toFixed(2) + ' °C</p><p id="f">Average ' + avg_f.toFixed(2) + ' °F</p>';
+	output = t_avg + t_cont;
 	document.getElementById("diiiv").innerHTML = output;
 };
 call.open("GET", file, true);
